3eb13d0b0d7bcc8adee2e7cb0b1d0dd381a2d25b
Andrii Marynych <Andrii.Marynych@wolterskluwer.com>
2021-01-04 21:04:37 +0200

    Add Robot Simulator and Kindergarten

---
+++ kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,26 @@
+﻿module KindergartenGarden
+
+type Plant = Violets | Grass | Radishes | Clover
+
+let matchPlant char =
+    match char with
+    | 'V' -> Violets
+    | 'R' -> Radishes
+    | 'C' -> Clover
+    | 'G' -> Grass
+
+let students =
+    [|"Alice"; "Bob"; "Charlie"; "David"; "Eve"; "Fred"; "Ginny"; "Harriet"; "Ileana"; "Joseph"; "Kincaid"; "Larry"|] 
+
+let studentOffset (student: string) =
+    2 * (Array.findIndex ((=) student) students)
+
+let plants (diagram: string) student =
+    let offset = studentOffset student
+    diagram.Split [|'\n'|]
+        |> Seq.map (fun line -> line.[offset..(offset+1)])
+        |> Seq.collect id
+        |> Seq.map matchPlant
+        |> List.ofSeq
+
+
---
+++ robot-simulator/RobotSimulator.fs
@@ -0,0 +1,41 @@
+﻿module RobotSimulator
+
+type Direction = North | East | South | West
+type Position = int * int
+type Robot = { Direction: Direction; Position: Position }
+
+let directions = [|North; East; South; West;|]
+let directionsCount = directions.Length
+
+let create direction position = { Direction = direction; Position = position }
+
+let getDirectionIndex direction = Array.findIndex ((=) direction) directions
+
+let getNewDirection direction offset =
+    let currentDirectionIndex = getDirectionIndex direction
+    let nextDirectionIndex = (directionsCount + currentDirectionIndex + offset) % directionsCount
+    directions.[nextDirectionIndex]
+
+let turn robot offset = create (getNewDirection robot.Direction offset) robot.Position 
+let turnLeft robot = turn robot -1
+let turnRight robot = turn robot 1
+
+let newPosition robot =
+     match robot.Direction with
+        | North -> (fst robot.Position, snd robot.Position + 1)
+        | East -> (fst robot.Position + 1, snd robot.Position)
+        | South -> (fst robot.Position, snd robot.Position - 1)
+        | West ->(fst robot.Position - 1, snd robot.Position)
+
+let advance robot = create robot.Direction (newPosition robot)
+
+let performInstruction robot instruction =
+    match instruction with
+    | 'R' -> turnRight robot
+    | 'L' -> turnLeft robot
+    | 'A' -> advance robot
+
+let rec move instructions robot = 
+    match instructions with
+    | "" -> robot
+    | _  -> move instructions.[1..] (performInstruction robot instructions.[0])
